cfe9b27de3c96eda4ef6399ce15261cb29de38fa
Melinda Wijaya <gmy4784@gmail.com>
2025-06-30 10:58:41 +0700

    feat: make pemasukan card at dashboard

--- app/(layout)/dashboard/page.jsx
+++ app/(layout)/dashboard/page.jsx
@@ -4,6 +4,7 @@ import StatisticChart from "@/components/organisms/StatisticChart";
 import InfoTotalCard from "@/components/organisms/InfoTotalCard";
 import StatusCard from "@/components/organisms/StatusCard";
 import TunggakanCard from "@/components/organisms/TunggakanCard";
+import PemasukanCard from "@/components/organisms/PemasukanCard";
 
 export default function dashboard() {
   return (
@@ -22,7 +23,18 @@ export default function dashboard() {
             <StatisticChart />
           </div>
           <div className="w-1/3">
-          <TunggakanCard/>
+            <TunggakanCard />
+          </div>
+        </div>
+        <div className="w-full flex space-x-8">
+          <div className="w-2/3 space-x-8 flex">
+            <div className="w-1/2">
+              <PemasukanCard />
+            </div>
+            <div className="w-1/2">
+            </div>
+          </div>
+          <div className="w-1/3">
           </div>
         </div>
       </div>
